452fed2e761f8930aa6171bfbc189e96aacca7af
adept159 <101689752+adept159@users.noreply.github.com>
2022-03-16 11:54:56 -0400

    Update email_generator.py

--- email_generator.py
+++ email_generator.py
@@ -1,3 +1,13 @@
+"""
+Created by Caleb Mingo
+2/14/2022
+
+The purpose of this program was originally to generate a specified amount of email with randomly generated names and passwords
+and store these usernames and passwords into a text file for future use.
+Due to "I am not a robot" I could not finish this project (probably a good thing for humanity lol).
+With chrome version 97 and a chrome driver installed, this programs enters in user information on yandex.com and inputs information 
+onto the website in an automated fashion.
+"""
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
@@ -12,6 +22,7 @@ last_name_list = ["henderson", "smith", "bateman", "shah", "little", "cunningham
 password_first_part_list = ["shadow", "silent", "zealous", "poisonous", "greedy", "lightning", "terminal", "dark", "blazing", "deadly", "wrathful"]
 password_second_part_list = ["Watcher", "Challenger", "King", "Jester", "Ninja", "Samurai", "Knight", "Fighter", "Killer", "Hero", "Zealot"]
 
+#Insert directory for webdriver here
 s = Service('/Users/calebmingo/Downloads/chromedriver')
 driver = webdriver.Chrome(service=s)
 
@@ -27,6 +38,7 @@ identifier = random.randint(1000, 9999)
 password = password_first_part_list[random.randint(0, 10)] + password_second_part_list[random.randint(0,10)] + str(identifier)
 username = first_name + last_name + str(identifier)
 
+#records email address and password into a text file
 email_database_text = 'email_collection.txt'
 sys.stdout = open(email_database_text, "a")
 print(username)
@@ -40,4 +52,12 @@ first_name_entry.send_keys(first_name)
 #enter in last name
 last_name_entry = driver.find_element(By.ID, "usernamereg-lastName")
 last_name_entry.send_keys(last_name)
+
+#enter in email
+email_entry = driver.find_element(By.ID, "email")
+email_entry.send_keys(username)
+
+#enter in password
+password_entry = driver.find_element(By.ID, "email")
+password_entry.send_keys(password)
 
